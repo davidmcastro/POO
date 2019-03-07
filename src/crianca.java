853b0c7ffbe72370fa7d8171d1b11ddee1d62804
04031491 <jerffsondavid@gmail.com>
2019-03-07 20:39:34 -0300

    Initial commit

---
+++ src/crianca.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// ESREVENDO CLASSES EM JAVA
+
+public class crianca {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//
+//    }
+    String corCabelo, corPele;
+    double altura;
+    int idade;
+    void brincar(){
+        System.out.println("Brincando...");
+    }
+    void correr(double metros){
+        System.out.println("Correndo ");
+    }
+}
